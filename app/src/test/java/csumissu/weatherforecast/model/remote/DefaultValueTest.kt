49fd9b150bc33474927876176ac23fb08aad2e6d
csumissu <csumissu@icloud.com>
2017-08-08 11:08:39 +0800

    add a small test

--- app/src/test/java/csumissu/weatherforecast/model/remote/DefaultValueTest.kt
+++ app/src/test/java/csumissu/weatherforecast/model/remote/DefaultValueTest.kt
@@ -2,6 +2,7 @@ package csumissu.weatherforecast.model.remote
 
 import csumissu.weatherforecast.model.Coordinate
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 /**
@@ -34,6 +35,14 @@ class DefaultValueTest {
         val coordinate = Coordinate(_longitude = 2.0)
         assertEquals(coordinate.latitude, 34.275, 0.0)
         assertEquals(coordinate.longitude, 2.0, 0.0)
+    }
+
+    @Test
+    fun testEquals() {
+        val c1 = Coordinate(1.0, 2.0)
+        val c2 = Coordinate(1.0, 2.0)
+        assertEquals(c1, c2)
+        assertTrue(c1 == c2)
     }
 
 }
